f1d6f9916e38dd7ea037a4f4d125e8f11909ed2f
LazaaR <Githeroku@outlook.com>
2025-01-20 12:21:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ from main import Bot
 
 Bot().run()
 
+#peerid
+import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1009999999999
+
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
